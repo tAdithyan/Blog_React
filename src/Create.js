20edac831b012e3a1952b5e2c38d9b3c388044d2
Adithyan T <adithyanttech@gmail.com>
2024-06-20 13:16:50 +0530

    submit Events

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,17 @@ const Create = () => {
   const[title,settitle]=useState('');
   const[Author,setAuthor]=useState('')
   const[body,setbody]=useState('')
+   
+  const handleSubmit =(e) =>{
+    e.preventDefault();
+    const blog ={title,Author,body}
+    console.log(blog);
 
+  }
   return ( 
     <div className="Create justify-center flex items-center flex-col gap-4 mt-5">
       <h1 className="text-xl text-red-500  ">Create new Blog</h1>
-      <form  className="flex-col flex ">
+      <form  className="flex-col flex " onSubmit={handleSubmit}>
         <label htmlFor="blogtitle" className="text-red-400 italic">Enter Title of the blog:</label>
         <input type="text" required  className="border border-red-400 rounded-xl p-2 m-2" value={title} onChange={(e)=>settitle(e.target.value)}/>
         <label htmlFor="blogtitle" className="text-red-400 italic">Enter Author of the blog:</label>
@@ -22,11 +28,7 @@ const Create = () => {
 
         
       </form>
-      <div className=" gap-4flex flex-col">
-      title:{title}
-      Author:{Author}
-      Content:{body}
-      </div>
+    
      
     </div>
    );
