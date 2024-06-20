355776a647dff687e4b43b7d06ad608ddf91b412
Adithyan T <adithyanttech@gmail.com>
2024-06-20 10:56:49 +0530

    Router  setup"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import HomePage from './Homepage';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   
   return (
+    <Router>
     <div >
+      <Switch>
+        <Route path="/">
     <HomePage/>
-
+    </Route>
+    </Switch>
+ 
+ 
     </div>
+    </Router>
   );
 }
 
