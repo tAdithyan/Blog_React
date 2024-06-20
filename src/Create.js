aad678f3c3292992d3ac4ceb90fca785110d668d
Adithyan T <adithyanttech@gmail.com>
2024-06-20 12:57:29 +0530

    git input basic

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
 const Create = () => {
+  const[title,settitle]=useState('');
+  const[Author,setAuthor]=useState('')
+  const[body,setbody]=useState('')
+
   return ( 
-    <div className="Create">
-      <h1>Create new Blog</h1>
+    <div className="Create justify-center flex items-center flex-col gap-4 mt-5">
+      <h1 className="text-xl text-red-500  ">Create new Blog</h1>
+      <form  className="flex-col flex ">
+        <label htmlFor="blogtitle" className="text-red-400 italic">Enter Title of the blog:</label>
+        <input type="text" required  className="border border-red-400 rounded-xl p-2 m-2" value={title} onChange={(e)=>settitle(e.target.value)}/>
+        <label htmlFor="blogtitle" className="text-red-400 italic">Enter Author of the blog:</label>
+        <input type="text" required  className="border border-red-400 rounded-xl p-2 m-2" value={Author} onChange={(e)=>setAuthor(e.target.value)}/>
+        <label htmlFor="blogtitle" className="text-red-400 italic">Enter Title of the blog:</label>
+        <textarea className="border border-red-400 rounded-xl p-2 m-2" required value={body} onChange={(e)=>setbody(e.target.value)}></textarea>
+   <div className=" justify-center flex">
+   <button className="border border-red-400 rounded-xl w-16 p-2 hover:text-red-800 bg-red-400 text-white">Create</button>
+
+   </div>
+
+        
+      </form>
+      <div className=" gap-4flex flex-col">
+      title:{title}
+      Author:{Author}
+      Content:{body}
+      </div>
+     
     </div>
    );
 }
