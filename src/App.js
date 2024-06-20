e87e1628eb5fa9c8a6fa07568c472ce9f32d681c
Adithyan T <adithyanttech@gmail.com>
2024-06-20 11:08:04 +0530

    Exact match Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import './App.css';
-import HomePage from './Homepage';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import "./App.css";
+import Create from "./Create";
+import HomePage from "./Homepage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  
   return (
     <Router>
-    <div >
-      <Switch>
-        <Route path="/">
-    <HomePage/>
-    </Route>
-    </Switch>
- 
- 
-    </div>
+      <div>
+        <Switch>
+          <Route exact  path="/">
+            <HomePage />
+          </Route>
+          <Route path="/Create">
+            <Create />
+          </Route>
+        
+        </Switch>
+      </div>
     </Router>
   );
 }
